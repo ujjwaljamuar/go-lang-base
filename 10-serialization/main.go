2ea5c25270a810d0848522fc3c275e6f23e3f7e9
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2026-03-08 01:36:45 +0530

    unmarshall using api call

--- 10-serialization/main.go
+++ 10-serialization/main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"bytes"
 	"encoding/gob"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"time"
 )
 
 func main() {
-	exampleItem()
+	// exampleItem()
+	getCartItem()
 }
 
+// gob
 type GoItem struct {
 	SKU   string
 	Name  string
@@ -45,4 +50,73 @@ func exampleItem() {
 	}
 
 	fmt.Printf("%s: %s\n", item2.SKU, item2.Name)
+}
+
+// JSON->GO  []byte  json.Unmarshal
+// GO->JSON  []byte  json.Marshal
+// JSON->GO  io.Reader  json.Decoder
+// GO->JSON  io.Writer  json.Encoder
+
+// JSON
+type CartItem struct {
+	Id           string  `json:"id"`
+	Index        int     `json:"index"`
+	Name         string  `json:"name"`
+	Image        string  `json:"image"`
+	CurrentPrice float64 `json:"current_price"`
+}
+
+func getCartItem() {
+	const url string = "https://ujjwaljamuar.github.io/JSONsAPIs/ProductItems.json"
+	client := http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	checkError(err)
+
+	resp, err := client.Do(req)
+	checkError(err)
+
+	defer resp.Body.Close()
+
+	respBytes, err := io.ReadAll(resp.Body)
+	checkError(err)
+	// content := string(respBytes)
+
+	// carItemsArr := make([]CartItem, 0)
+	// decoder := json.NewDecoder(strings.NewReader(content))
+
+	// _ , err = decoder.Token()
+	// checkError(err)
+
+	// var ct CartItem
+	// for decoder.More() {
+	// 	err := decoder.Decode(&ct)
+	// 	checkError(err)
+	// 	carItemsArr = append(carItemsArr, ct)
+	// }
+	var carItemsArr []CartItem
+	err = json.Unmarshal(respBytes, &carItemsArr)
+	checkError(err)
+
+	for _, ct := range carItemsArr {
+		id := ct.Id
+		index := ct.Index
+		name := ct.Name
+		image := ct.Image
+		currentPrice := ct.CurrentPrice
+
+		fmt.Printf("===================================\n")
+		fmt.Printf("id: %v\n", id)
+		fmt.Printf("index: %v\n", index)
+		fmt.Printf("name: %v\n", name)
+		fmt.Printf("image: %v\n", image)
+		fmt.Printf("currentPrice: %v\n", currentPrice)
+		fmt.Printf("===================================\n")
+	}
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
